41f48b2509ed195c67e5cfb8a93492bdfa7e241c
jinbao.he <jinbao.he@d-robotics.cc>
2024-08-09 17:29:04 +0800

    fix(vp_sensors): add ov5647 sensor config option
    Signed-off-by: jinbao.he <jinbao.he@d-robotics.cc>

--- debian/app/multimedia_samples/vp_sensors/vp_sensors.c
+++ debian/app/multimedia_samples/vp_sensors/vp_sensors.c
@@ -27,6 +27,7 @@ extern vp_sensor_config_t imx415_linear_3480x2160_raw10_30fps_4lane;
 extern vp_sensor_config_t sc202cs_linear_1600x1200_raw10_30fps_1lane;
 extern vp_sensor_config_t irs2381c_linear_224x1903_raw12_5fps_2lane;
 extern vp_sensor_config_t imx219_linear_1920x1080_raw10_30fps_2lane;
+extern vp_sensor_config_t ov5647_linear_1920x1080_raw10_30fps_2lane;
 
 vp_sensor_config_t *vp_sensor_config_list[] = {
 	&sc1330t_linear_1280x960_raw10_30fps_1lane,
@@ -41,6 +42,7 @@ vp_sensor_config_t *vp_sensor_config_list[] = {
 	&sc202cs_linear_1600x1200_raw10_30fps_1lane,
 	&irs2381c_linear_224x1903_raw12_5fps_2lane,
 	&imx219_linear_1920x1080_raw10_30fps_2lane,
+	&ov5647_linear_1920x1080_raw10_30fps_2lane,
 };
 
 uint32_t vp_get_sensors_list_number() {

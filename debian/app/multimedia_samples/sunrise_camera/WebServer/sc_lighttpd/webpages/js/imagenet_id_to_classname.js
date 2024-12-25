d850e923ff7d641fb520d304b4f764a96009737b
xin03.liu <xin03.liu@horizon.cc>
2024-12-25 10:40:05 +0800

    feat(license): add license
    Signed-off-by: xin03.liu <xin03.liu@horizon.cc>

--- debian/app/multimedia_samples/sunrise_camera/WebServer/sc_lighttpd/webpages/js/imagenet_id_to_classname.js
+++ debian/app/multimedia_samples/sunrise_camera/WebServer/sc_lighttpd/webpages/js/imagenet_id_to_classname.js
@@ -1,3 +1,17 @@
+// Copyright (c) 2024，D-Robotics.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 const id_to_classname = [
     { id: "0", code: "n01440764", name: " 鱼, tench, Tinca tinca" },
     { id: "1", code: "n01443537", name: " 鱼, goldfish, Carassius auratus" },
